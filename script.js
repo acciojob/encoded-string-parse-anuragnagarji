2be7953219f883d92d137761324c5744b33967ce
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2023-12-30 13:31:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const parseCode = (str) => {
-  let valArr=str.split("000");
-	let obj={firstName:valArr[0],lastName:valArr[1],id:valArr[2]}
-return obj;
-};
+    let valArr1=str.split("0");
+    let valArr=[];
+    for(let i in valArr1){
+      if(valArr1[i]!=""){
+       valArr.push(valArr1[i]);
+      }}
+    
+   // console.log(valArr);
+      let object1={firstName:valArr[0],lastName:valArr[1],id:valArr[2]}
+  return object1;
+  };
 
 // Do not change the code below
 const str = prompt("Enter str: ");
